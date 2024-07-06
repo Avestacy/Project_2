d997a85c873ba68d640a57427f4101296fd4e65b
Avestacy <162714486+Avestacy@users.noreply.github.com>
2024-07-06 14:41:06 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        int priceTicket  = 1000;  //цена за один билет
+        int priceTicket = 1000;  //цена за один билет
         int priceMile = 20;  //стоимость одной мили
 
         int bonusMiles = (int) (priceTicket / priceMile);
 
-        System.out.println("Начисленных миль после расчете: "+ bonusMiles);
+        System.out.println("Начисленных миль после расчете: " + bonusMiles);
     }
 }
